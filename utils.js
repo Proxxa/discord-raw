b74cfa20eb3d4a52489d6910d54a612c2b24fe67
Proxxa <yorip625@gmail.com>
2023-06-01 18:22:24 -0400

    remove JS files

--- utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isDefined = exports.bypassGuard = void 0;
-const bypassGuard = (x) => true;
-exports.bypassGuard = bypassGuard;
-const isDefined = (x) => x != undefined;
-exports.isDefined = isDefined;
